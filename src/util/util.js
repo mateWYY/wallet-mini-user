45c0673f352fb3fa5ff0d6ffc8c20332f32a675c
wangxiaooyan <18856037875@163.com>
2018-07-06 19:34:06 +0800

    首页 今日福袋 我的红包  搜索

--- src/util/util.js
+++ src/util/util.js
@@ -2,9 +2,9 @@
 import wepy from 'wepy';
 // const host = 'https://coupon.test.mihutime.com';
 const host = 'https://itimego.com'
-let token = wx.getStorageSync('token');
+let token = wx.getStorageSync('token')
 const wxRequest = (params = {}, url) => {
-  let token = wepy.getStorageSync('token');
+  let token = wepy.getStorageSync('token')
   return new Promise((resolve, reject) => {
     wx.request({
       url: url, // 仅为示例，并非真实的接口地址
